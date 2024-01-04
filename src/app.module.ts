1ed72ad462ef8886bb4b6078d275e7fcea217d66
Marcus  Roza <mavirolero@gmail.com>
2024-01-03 22:08:38 -0300

    app module

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@infra/database/database.module';
+import { HttpModule } from '@infra/http/http.module';
+import { ConfigModule } from '@nestjs/config';
+import { BullConfigModule } from './infra/config/bull.module';
+import { MailerConfigModule } from './infra/config/mailer.module';
+import { MessageController } from 'src/app.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    BullConfigModule,
+    MailerConfigModule,
+    HttpModule,
+    DatabaseModule,
+   
+  ],
+  controllers: [MessageController],
+  providers: [],
+})
+export class AppModule {}
